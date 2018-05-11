48f7889b7970852922c66e5ad9ae74c35b5a2e3c
Jack Burtis <johnburtis2019@u.northwestern.edu>
2018-05-11 13:48:36 -0500

    first

---
+++ ipo2.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>GreenMove Home</title>
+  </head>
+
+  <body>
+  <h1> Greenmove </h1>
+  <h2> Responsible Disposal Helper </h2>
+  Enter the name of a common household object and we will tell you 
+  how to dipose of it in an enviornmentally friendly way.
+  <br>
+  <input type="text" name="object"><br>
+  <button type="button" > Submit </button>
