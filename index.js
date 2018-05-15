0fd3b9651642eaf8becb5717cb4716e9cc45cdd1
[jmidvidy] <[jmidvidy@gmail.com]>
2018-05-15 11:51:02 -0500

    added basic js

---
+++ index.js
@@ -0,0 +1,10 @@
+  function showInfo(item){
+
+ 	document.getElementById('RIRight-card-header').innerHTML = item;
+
+ 	var card_text = item + " info";
+
+ 	 document.getElementById('RIRight-card-text').innerHTML = card_text;
+
+
+ }
